ee4883feab7e6d5ea93b0a3927848e158aadf7e7
Antonio <ntoniohu@gmail.com>
2018-10-28 16:14:09 +0100

    Update code

--- EDA.R
+++ EDA.R
@@ -1,7 +1,7 @@
-## ---- load_libraries and settings
-setwd("/home/antortjim/MEGA/Kaggle/Datrik")
+## ---- load_libraries ----
 library(ggplot2)
 library(dplyr)
+library(magrittr)
 library(waffle)
 library(tidyr)
 library(tibble)
@@ -9,14 +9,25 @@ library(ade4)
 library(data.table)
 library(stringr)
 library(FactoMineR)
-library(limma)
-library(DiscriMiner)
+library(cowplot)
+library(pheatmap)
+library(kableExtra)
 plot_dir <- "plots"
 output_data <- "proc_data"
+rdata_dir <- "RData"
+tables_dir <- "tables"
 
-## ---- load_data
+## ---- load_data ----
 datos <- read.table("datos.csv", sep = ",", header=T, stringsAsFactors = F)
 datos <- datos[, c(colnames(datos)[1:4], colnames(datos)[5:(ncol(datos)-1)] %>% sort,"Y")]
+write(x = kable(x = datos %>% colnames %>% strsplit(., split = "_") %>%
+                  lapply(., function(x) x[[1]]) %>%
+                  unlist %>% table %>% t,
+                format = "latex", digits = 2),
+      file = file.path(tables_dir, "data_summary.tex")
+      )
+
+
 datos$edad_integer <- str_match(string = datos$edad,
                                 pattern = "\\[\\d{2}-(\\d{2})\\)") %>%
   .[, 2] %>%
@@ -28,10 +39,19 @@ y_train <- select(train_set, Y) %>% mutate(Y=as.factor(Y))
 
 test_set <- datos[is.na(datos["Y"]), ]
 x_test <- test_set %>% select(-Y)
-y_test <- test_set %>% select(Y) %>% mutate(Y=as.factor(Y))
 
-## ---- plots1
-x_train$raza %>% table
+## ---- load_data_save ----
+datasets <- list(x_train = x_train, y_train = y_train, x_test = x_test)
+save("datasets", file = file.path("RData", "datasets.RData"))
+
+## ---- load_data_load ----
+load(file = "RData/datasets.RData")
+x_train <- datasets$x_train
+x_test <- datasets$x_test
+y_train <- datasets$y_train
+
+
+## ---- plots1 ----
 race <- as.character(x_train$raza)
 race <- table(race)
 race <- 200*race/(max(race)) %>% sort
@@ -47,20 +67,16 @@ race_colors <- c(
   "Asian"="yellow"
 )[names(race)]
 
-p <- waffle(race, rows = 20, colors = race_colors)
-ggsave(p, filename = file.path(plot_dir, "race_waffle.png"))
+p1 <- waffle(race, rows = 20, colors = race_colors)
+ggsave(p1, filename = file.path(plot_dir, "race_waffle.png"))
 
-p <- ggplot(data=x_train,
-       mapping=aes(x = edad, y=..count../1000)
-       ) +
-  geom_bar() +
-  labs(y = "kCounts", x="Age") +
-  ggtitle("Age distribution")
-  # scale_x_continuous(breaks = sort(unique(histogram_data$edad)))
-p
-ggsave(p, filename = file.path(plot_dir, "age_histogram.png"))
+p2 <- ggplot(data=x_train, mapping=aes(x = edad, y=..count../1e3))
+p2 <- p2 + geom_bar() + labs(y = "kCounts", x="Age") + ggtitle("Age distribution")
+ggsave(p2, filename = file.path(plot_dir, "age_histogram.png"))
+p3 <- plot_grid(ncol=2, p1 + theme(legend.position = "top"), p2, labels = "AUTO")
+ggsave(p3, filename = file.path(plot_dir, "visualize_categories.png"), width=14)
 
-## ---- preprocess_function
+## ---- preprocess_function ----
 preprocess_data <- function(x_train, x_test, etiquettes=FALSE) {
   
   if(etiquettes) {
@@ -218,44 +234,102 @@ preprocess_data <- function(x_train, x_test, etiquettes=FALSE) {
   return(processed_datasets)
 }
 
-## ---- preprocess
-quantit <- c("farmaco", "ordinal", "edad_integer", "counter")
-qualit <- c("raza", "binary", "nominal", "sexo")
-# if(etiquettes_in) qualit <- c(qualit, "et")
+## ---- preprocess ----
+quanti <- c("farmaco", "ordinal", "edad_integer", "counter")
+quali <- c("raza", "binary", "nominal", "sexo")
 processed_datasets <- preprocess_data(x_train, x_test, etiquettes = FALSE)
 proc_x_train <- processed_datasets$train
 proc_x_test <- processed_datasets$test
-rm(processed_datasets)
-quanti_index <- lapply(quantit, function(x) grep(x = colnames(proc_x_train), pattern = x)) %>% unlist
-qualit_index <- lapply(qualit, function(x) grep(x = colnames(proc_x_train), pattern = x)) %>% unlist
+quanti_index <- lapply(quanti, function(x) grep(x = colnames(proc_x_train), pattern = x)) %>% unlist
+quali_index <- lapply(quali, function(x) grep(x = colnames(proc_x_train), pattern = x)) %>% unlist
+## ---- preprocess_etiqueta ----
 processed_datasets <- preprocess_data(x_train, x_test, etiquettes = TRUE)
+quanti_index_full <- lapply(quanti, function(x) grep(x = colnames(processed_datasets$train), pattern = x)) %>% unlist
+quali_index_full <- lapply(c(quali, "et"), function(x) grep(x = colnames(processed_datasets$train), pattern = x)) %>% unlist
 
-## ---- PCA
+write.table(x = processed_datasets$train, file = file.path(output_data, "x_train.csv"),
+            sep = ",", row.names = x_train$identificador, quote = F, col.names = T)
+write.table(x = processed_datasets$test, file = file.path(output_data, "x_test.csv"),
+            sep = ",", row.names = x_test$identificador, quote = F, col.names = T)
+
+## ---- preprocess_save ----
+# rm(processed_datasets)
+save(list = c("quanti", "quali",
+              "quanti_index", "quali_index",
+              "quanti_index_full", "quali_index_full",
+              "proc_x_train", "proc_x_test", "processed_datasets"),
+     file = file.path(rdata_dir, "preprocessed.RData")) 
+
+## ---- preprocess_load ----
+load(file = file.path(rdata_dir, "preprocessed.RData"))
+
+## ---- PCA ----
 res.pca <- prcomp(x = proc_x_train[, quanti_index], center = T, scale. = T)
-barplot(1:length(res.pca$sdev), cumsum(res.pca$sdev))
+
+pca_barplot_data <- data.frame(PC = 1:length(res.pca$sdev),
+                               var = res.pca$sdev**2)
+plot_grid(ncol = 2, rel_widths = c(1,1),
+          ggplot(data = pca_barplot_data, aes(x=PC, y=var)) +
+            geom_bar(stat="identity") + labs(y = "Variance"),
+          ggplot(data = pca_barplot_data, aes(x=PC, y=cumsum(var)/sum(var))) +
+            geom_bar(stat="identity") + labs(y = "Fraction cumulative variance")
+)
+
 
 
 pca_data <- cbind(x_train[, grep(pattern = "et", x = colnames(x_train), invert = T)],
                   as.data.frame(res.pca$x), y_train)
 
-## ---- PCA_visualization
-ggplot(data = pca_data,
-       mapping = aes(x = PC1, y = PC2, col = raza)) + geom_point(size=.1)
+## ---- PCA_visualization ----
+p1 <- ggplot(
+  data = pca_data,
+  mapping = aes(x = PC1, y = PC2, col = raza)
+  ) + geom_point(size=.1) +
+  guides(col=F) + ggtitle("Raza")
 
-ggplot(data = pca_data,
-       mapping = aes(x = PC1, y = PC2, col = sexo)) + geom_point(size=.1)
+p2 <- ggplot(
+  data = pca_data,
+  mapping = aes(x = PC1, y = PC2, col = sexo)
+  ) + geom_point(size=.1) +
+  guides(col=F) + ggtitle("Sexo")
 
-ggplot(data = pca_data,
-       mapping = aes(x = PC1, y = PC2, col = binary_1)) + geom_point(size=.1)
+p3 <- ggplot(
+  data = pca_data,
+  mapping = aes(x = PC1, y = PC2, col = edad_integer)
+  ) + geom_point(size=.1) +
+  guides(col=F) + ggtitle("Edad")
 
-ggplot(data = pca_data,
-       mapping = aes(x = PC1, y = PC2, col = edad_integer)) + geom_point(size=.05)
+p4 <- ggplot(
+  data = pca_data,
+  mapping = aes(x = PC1, y = PC2, col = Y)
+  ) + geom_point(size=.1) +
+  guides(col=F) + ggtitle("Y")
 
-ggplot(data = pca_data,
-       mapping = aes(x = PC1, y = PC2, col = Y)) + geom_point(size=.05)
+p <- plot_grid(nrow=2, ncol=2, p1,p2,p3,p4, labels="AUTO")
+
+ggsave(
+  filename = file.path(plot_dir, "PCA_multicategory.png"),
+  plot = p
+  )
 
 
-## ---- MCA_function
+## ---- MCA_functions ----
+prepare_projection_data <- function(x, y, proc_x, quali_index, quanti_index) {
+  res.mca <- MCA(
+    X = proc_x, ncp = ncp,
+    quanti.sup = quanti_index, graph = F
+  )
+  cats <- apply(proc_x[,quali_index], 2, function(x) nlevels(as.factor(x)))
+  mca_df <- extract_mca_data(proc_x, x, cats, res.mca)
+  mca_df$obs$Y <- y$Y
+  sup_proj_data <- cbind(proc_x[, quanti_index],
+                         mca_df$obs %>% select(`Dim 1`:`Dim 5`))
+  
+  colnames(sup_proj_data)[colnames(sup_proj_data) %>% grep(pattern = "Dim")] <- paste0("MCA_", 1:ncp)
+  return(list(vars = mca_df$vars, obs = mca_df$obs, proj=sup_proj_data))
+}
+
+
 extract_mca_data <- function(proc_data, x, cats, mca) {
   mca_vars_df <- data.frame(mca$var$coord, Variable = rep(names(cats), cats))
   
@@ -265,78 +339,103 @@ extract_mca_data <- function(proc_data, x, cats, mca) {
     lapply(function(x) x[[1]]) %>% unlist
   
   mca_obs_df <- cbind(mca$ind$coord,
-                      x[, lapply(qualit, function(y) grep(x = colnames(x), pattern = y)) %>% unlist]
+                      edad_integer = x[,"edad_integer"],
+                      x[, lapply(quali, function(y) grep(x = colnames(x), pattern = y)) %>% unlist]
   )
   return(list(vars = mca_vars_df, obs = mca_obs_df))
 }
 
-## ---- MCA
+make_mca_plots <- function(train, test, point_size, prefix="") {
+  p1 <- ggplot(data=train$vars, 
+               aes(x = Dim.1, y = Dim.2, col = Type))
+  p1 <- p1 + geom_hline(yintercept = 0, colour = "gray70")
+  p1 <- p1 + geom_vline(xintercept = 0, colour = "gray70")
+  p1 <- p1 + geom_point(size=2)
+  p1 <- p1 + ggtitle("MCA plot of variables using R package FactoMineR")
+  
+  p2 <- ggplot(data = train$obs, aes(x = `Dim 1`, y = `Dim 2`, col = raza))
+  p2 <- p2 + geom_point(size=point_size) + facet_wrap(~raza)
+  p2 <- p2 + ggtitle("MCA plot facetted by race") 
+  
+  p3 <- ggplot(data = train$obs, aes(x = `Dim 1`, y = `Dim 2`))
+  p3 <- p3 + geom_point(size=point_size) + facet_wrap(~Y)
+  p3 <- p3 + ggtitle("MCA plot facetted by Y") 
+  
+  p4 <- ggplot(data = test$obs, aes(x = `Dim 1`, y = `Dim 2`))
+  p4 <- p4 + geom_point(size=point_size) + ggtitle("MCA plot of the test set") 
+  
+  p5 <- ggplot(data = train$obs, aes(x = `Dim 1`, y = `Dim 2`, col = raza))
+  p5 <- p5 + geom_point(size=point_size) + facet_wrap(~Y)
+  p5 <- p5 + ggtitle("MCA plot facetted by Y") 
+  
+  p6 <- ggplot(data = train$obs, aes(x = `Dim 1`, y = `Dim 2`, col = as.factor(edad_integer)))
+  p6 <- p6 + geom_point(size=point_size) + facet_wrap(~Y)
+  p6 <- p6 + ggtitle("MCA plot facetted by Y")
+  p6 <- p6 + scale_color_discrete() + guides(col = guide_legend(title = "Edad"))
+  
+  p7 <- plot_grid(p3, p4, ncol=2)
+  
+  ggsave(plot = p1, filename = file.path(plot_dir, paste0(prefix, "mca_variables.png")))
+  ggsave(plot = p2, filename = file.path(plot_dir, paste0(prefix, "mca_obs_race_facet.png")))
+  ggsave(plot = p3, filename = file.path(plot_dir, paste0(prefix, "mca_obs_Y_facet.png")))
+  ggsave(plot = p4, filename = file.path(plot_dir, paste0(prefix, "mca_obs_test.png")))
+  ggsave(plot = p5, filename = file.path(plot_dir, paste0(prefix, "mca_obs_Y_facet_raza_col.png")))
+  ggsave(plot = p6, filename = file.path(plot_dir, paste0(prefix, "mca_obs_Y_facet_edad_col.png")))
+  ggsave(plot = p7, filename = file.path(plot_dir, paste0(prefix, "mca_obs_Y_facet_all.png")), width=14)
+  return(list(p1,p2,p3,p4,p5,p6,p7))
+}
+
+## ---- MCA ----
 ncp <- 5
-train_res.mca <- MCA(
-  X = proc_x_train, ncp = ncp,
-  quanti.sup = quanti_index
-)
 
+# Without etiqutas
+train_sup_proj_data <- prepare_projection_data(x_train, y_train, proc_x_train, quali_index, quanti_index)
 unique_cols <- apply(proc_x_test, 2, function(x) length(unique(x)) == 1) %>% which
-quanti_index_2 <- lapply(quantit, function(x) grep(x = colnames(proc_x_train[,-unique_cols]), pattern = x)) %>% unlist
-proc_x_test_mca <- proc_x_test[, -unique_cols]
+quanti_index_mca <- lapply(quanti, function(x) grep(x = colnames(proc_x_test[,-unique_cols]), pattern = x)) %>% unlist
+quali_index_mca <- lapply(quali, function(x) grep(x = colnames(proc_x_test[,-unique_cols]), pattern = x)) %>% unlist
+test_sup_proj_data <- prepare_projection_data(x_test, NULL, proc_x_test[, -unique_cols], quali_index_mca, quanti_index_mca)
+  
+
+# With etiquetas
+train_sup_proj_data_full <- prepare_projection_data(x_train, y_train, processed_datasets$train, quali_index_full, quanti_index_full)
+unique_cols <- apply(processed_datasets$test, 2, function(x) length(unique(x)) == 1) %>% which
+quanti_index_full_mca <- lapply(quanti, function(x) grep(x = colnames(processed_datasets$test[,-unique_cols]), pattern = x)) %>% unlist
+quali_index_full_mca <- lapply(c("et", quali), function(x) grep(x = colnames(processed_datasets$test[,-unique_cols]), pattern = x)) %>% unlist
+test_sup_proj_data_full <- prepare_projection_data(x_test, NULL, processed_datasets$test[, -unique_cols], quali_index_full_mca, quanti_index_full_mca)
 
 
-test_res.mca <- MCA(
-  X = proc_x_test_mca, ncp = ncp,
-  quanti.sup = quanti_index_2
+## ---- MCA_save ----
+save(list = c("train_sup_proj_data", "test_sup_proj_data",
+              "train_sup_proj_data_full", "test_sup_proj_data_full"),
+     file = file.path(rdata_dir, "mca_dfs.RData"))
+
+## ---- MCA_load ----
+load(file.path(rdata_dir, "mca_dfs.RData"))
+
+
+## ---- MCA_visualization ----
+point_size <- .5
+plots_without <- make_mca_plots(train_sup_proj_data, test_sup_proj_data, point_size)
+plots_with <- make_mca_plots(train_sup_proj_data_full, test_sup_proj_data_full, point_size, prefix = "etiqueta_")
+ggsave(filename = file.path(plot_dir, "mca_variables_combined.png"),
+       plot = plot_grid(plots_without[[1]], plots_with[[1]], labels="AUTO"),
+       width = 14
+       )
+
+ggsave(filename = file.path(plot_dir, "mca_obs_Y_facet_all_combined.png"),
+       plot = plot_grid(plots_without[[7]], plots_with[[7]], nrow = 2, labels="AUTO"),
+       width = 14, height = 14
 )
 
-
-cats <- apply(proc_x_train[,qualit_index], 2, function(x) nlevels(as.factor(x)))
-train_mca_df <- extract_mca_data(proc_x_train, x_train, cats, train_res.mca)
-train_mca_df$obs$Y <- y_train$Y
-cats <- apply(proc_x_test_mca[,qualit_index_2], 2, function(x) nlevels(as.factor(x)))
-test_mca_df <- extract_mca_data(proc_x_test_mca, x_test, cats, test_res.mca)
-
-## ---- MCA_visualization
-p <- ggplot(data=train_mca_df$vars, 
-       aes(x = Dim.1, y = Dim.2, col = Type)) +
-  geom_hline(yintercept = 0, colour = "gray70") +
-  geom_vline(xintercept = 0, colour = "gray70") +
-  geom_point(size=1) +
-  # geom_text(aes(label = rownames(mca1_vars_df))) + guides(col=F)
-  ggtitle("MCA plot of variables using R package FactoMineR")
-
-p
-ggsave(plot = p, filename = file.path(plot_dir, "mca_variables.png"))
-
-
-p <- ggplot(data = train_mca_df$obs, aes(x = `Dim 1`, y = `Dim 2`, col = raza)) +
-  geom_point(size=.1) +
-  facet_wrap(~raza)
-p
-ggsave(plot = p, filename = file.path(plot_dir, "mca_obs_race_facet.png"))
-
-p <- ggplot(data = train_mca_df$obs, aes(x = `Dim 1`, y = `Dim 2`, col = Y)) +
-  geom_point(size=.1) +
-  facet_wrap(~Y)
-p
-ggsave(plot = p, filename = file.path(plot_dir, "mca_obs_Y_facet.png"))
-
-p <- ggplot(data = test_mca_df$obs, aes(x = `Dim 1`, y = `Dim 2`)) +
-  geom_point(size=.1)
-p
-
-
-## ---- MCA_features
-train_sup_proj_data <- cbind(proc_x_train[, quanti_index],
-                             train_mca_df$obs %>% select(`Dim 1`:`Dim 5`))
-test_sup_proj_data <- cbind(proc_x_test[, quanti_index],
-                            test_mca_df$obs %>% select(`Dim 1`:`Dim 5`))
-
-## ---- supervised_projections
+## ---- supervised_projections ----
 ## lda <- plotRLDF(
 ##   t(sup_proj_data),
 ##   labels.y = y_train$Y,
 ##   trend = TRUE, robust = TRUE
 ##   )
 ## str(lda)
+
+
 
 
 ## Perform PLSDA
@@ -349,26 +448,41 @@ test_sup_proj_data <- cbind(proc_x_test[, quanti_index],
 # 
 # qplot(data=as.data.frame(plsda$components), x=t1, y=t2, geom=c("point"), color=y_train$Y)
 
-## ---- Heatmap
-idx <- sample(x = 1:nrow(train_sup_proj_data),size = 1e3)
-heatmap <- pheatmap::pheatmap(train_sup_proj_data[idx, ] %>% as.matrix,
-                   annotation_row  = x_train[idx, ] %>% select(raza, sexo, edad_integer),
-                   # annotation_col=y_train$Y[idx],
-                   scale="row", filename = "heatmap.png")
-save(file = "heatmap.rda", heatmap)
+## ---- heatmap ----
+idx <- sample(x = 1:nrow(train_sup_proj_data$proj),size = 1e3)
+heatmap_data <- train_sup_proj_data$proj[idx, ] %>% as.matrix
+heatmap <- pheatmap(heatmap_data,
+                    annotation_row  = cbind(select(x_train[idx, ], raza, sexo, edad_integer), Y = y_train[idx,"Y"]),
+                    scale="row", filename = file.path(plot_dir, "heatmap.png"))
 
-## ---- export_data
+write(x = kable(x = train_sup_proj_data$proj[,train_sup_proj_data$proj %>% colnames %>%
+                                               grep(pattern = "counter", x = .)] %>%
+                  lapply(., function(x) length(table(x))) %>% unlist %>% sort %>% rev),
+      file = file.path(tables_dir, "counters_overview.tex"))
+
+
+## ---- heatmap_save ----
+save(list = c("heatmap"),
+     file = file.path(rdata_dir, "heatmap.RData"))
+
+## ---- heatmap_load ----
+load(file.path(rdata_dir, "heatmap.RData"))
+
+## ---- export_data ----
 # Save processed datasets without one-hot encoding data but with mca features
-write.table(x = train_sup_proj_data, file = file.path(output_data, "x_train_mca.csv"),
+write.table(x = train_sup_proj_data$proj, file = file.path(output_data, "x_train_mca.csv"),
             sep = ",", row.names = x_train$identificador, quote = F, col.names = T)
-write.table(x = test_sup_proj_data, file = file.path(output_data, "x_test_mca.csv"),
+write.table(x = test_sup_proj_data$proj, file = file.path(output_data, "x_test_mca.csv"),
             sep = ",", row.names = x_test$identificador, quote = F, col.names = T)
+
+write.table(x = train_sup_proj_data_full$proj, file = file.path(output_data, "x_train_mca_full.csv"),
+            sep = ",", row.names = x_train$identificador, quote = F, col.names = T)
+write.table(x = test_sup_proj_data_full$proj, file = file.path(output_data, "x_test_mca_full.csv"),
+            sep = ",", row.names = x_test$identificador, quote = F, col.names = T)
+
 # Save label to a separate file
 write.table(x = y_train, file = file.path(output_data, "y_train.csv"),
             sep = ",", row.names = x_train$identificador, quote = F, col.names = T)
 
-# Save processed datasets containing etiquetas too
-write.table(x = processed_datasets$train, file = file.path(output_data, "x_train.csv"),
-            sep = ",", row.names = x_train$identificador, quote = F, col.names = T)
-write.table(x = processed_datasets$test, file = file.path(output_data, "x_test.csv"),
-            sep = ",", row.names = x_test$identificador, quote = F, col.names = T)
+## ---- session_info ----
+sessionInfo()
